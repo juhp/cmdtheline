juhp/cmdtheline

commit f5294f1b56dce20a1ffef998441c8407c744811f

make lib and tests compile with -Wall & -Werror

src/System/Console/CmdTheLine/Manpage.hs
@@ -19,7 +19,6 @@ import Control.Exception ( handle, throw, IOException, SomeException )
 
 import Data.Maybe ( catMaybes )
 import Data.Char  ( isSpace )
-import Data.List  ( subsequences, words )
 
 import Text.Parsec
 import Text.PrettyPrint hiding ( char )
@@ -27,6 +26,7 @@ import Text.PrettyPrint hiding ( char )
 type Subst = [( String, String )] -- An association list of
                                   -- ( replacing, replacement ) pairs.
 
+paragraphIndent, labelIndent :: Int
 paragraphIndent = 7
 labelIndent     = 4
 
@@ -52,17 +52,11 @@ substitute esc assoc = either (error . show) id . parse subst ""
 
   scan = many $ try (string "\\$") <|> try replace <|> pure <$> anyChar
 
-  replace = do
-    string "$("
-    replacement <- try escape <|> choice replacers <|> safeChars
-    char ')'
-    return replacement
+  replace = string "$(" >> content <* char ')'
+    where
+    content = try escape <|> choice replacers <|> safeChars
 
-  escape = do
-    c <- anyChar
-    char ','
-    str <- try replace <|> safeChars
-    return $ esc c str
+  escape = esc <$> (anyChar <* char ',') <*> try replace <|> safeChars
 
   safeChars = many1 $ satisfy (/= ')')
 

test/Main.hs
@@ -59,52 +59,50 @@ withInput input action = do
     return v)
 
 unwrapFB, unwrapAr, unwrapCP, unwrapCi :: [String] -> IO (Either EvalExit (IO ()))
-
-unwrapFB args = mute $ unwrap args ( FB.term, FB.termInfo )
-unwrapAr args = mute $ unwrap args ( Ar.arithTerm, Ar.termInfo )
-unwrapCP args = mute $ unwrap args ( CP.term, CP.termInfo )
-unwrapCi args = mute $
-  unwrapChoice args Ci.defaultTerm [ Ci.rotTerm, Ci.morseTerm ]
+unwrapFB args = unwrap args ( FB.term, FB.termInfo )
+unwrapAr args = unwrap args ( Ar.arithTerm, Ar.termInfo )
+unwrapCP args = unwrap args ( CP.term, CP.termInfo )
+unwrapCi args = unwrapChoice args Ci.defaultTerm [ Ci.rotTerm, Ci.morseTerm ]
 
 tests :: [Test]
 tests =
   -- With FizzBuzz we'll test the different forms of option assignment and
   -- flags.
   [ testGroup "FizzBuzz"
-    [ testCase "w/o args" . assert $ isRight <$> unwrapFB []
-    , testCase "w/ good args" . assert $
+    [ testCase " w/o args" . assert $ isRight <$> unwrapFB []
+    , testCase " w/ good args" . assert $
       isRight <$> unwrapFB [ "-q", "-s", "-v"
                            , "-f", "bob", "--buzz", "ann", "-t20"
                            ]
-    , testCase "w/ bad args" . assert $ isLeft <$> unwrapFB [ "-zork" ]
+    , testCase " w/ bad args" . assert $ isLeft <$> unwrapFB [ "-zork" ]
     ]
 
   -- With arith we'll test 'posRight' 'pos' positional argument partitioning.
   , testGroup "arith"
-    [ testCase "w/o args" . assert $ isLeft <$> unwrapAr []
-    , testCase "w/ good args many" . assert $
+    [ testCase " w/o args" . assert $ isLeft <$> unwrapAr []
+    , testCase " w/ good args many" . assert $
       isRight <$> unwrapAr [ "x^2+y*1", "x=2", "y=(11-1)/5" ]
-    , testCase "w/ good args one" . assert $
+    , testCase " w/ good args one" . assert $
       isRight <$> unwrapAr [ "x^2", "x=2" ]
-    , testCase "w/ bad args" . assert $
+    , testCase " w/ bad args" . assert $
       isLeft <$> unwrapAr [ "-pretty", "x^2", "x=2" ]
     ]
 
   -- With cipher we'll test subcommands.
   , testGroup "cipher"
-    [ testCase "w/o args" . assert $ isLeft <$> unwrapCi []
-    , testCase "morse" . assert $
+    [ testCase " w/o args" . assert $ isLeft <$> unwrapCi []
+    , testCase " morse" . assert $
       isRight <$> withInput ["bob"] (unwrapCi [ "morse" ])
-    , testCase "rot" . assert $
+    , testCase " rot" . assert $
       isRight <$> withInput ["bob"] (unwrapCi [ "rot" ])
     ]
 
   -- With cp we'll test 'revPosLeft' 'revPos' positional argument partitioning.
   , testGroup "cp"
-    [ testCase "w/o args" . assert $ isLeft <$> unwrapCP []
-    , testCase "w/ good args many" . assert $
+    [ testCase " w/o args" . assert $ isLeft <$> unwrapCP []
+    , testCase " w/ good args many" . assert $
       isRight <$> unwrapCP [ "-d", "cmdtheline.cabal", "LICENSE", "test" ]
-    , testCase "w/ good args one" . assert $
+    , testCase " w/ good args one" . assert $
       isRight <$> unwrapCP [ "-d", "cmdtheline.cabal", "foo" ]
     ]
   ]

src/System/Console/CmdTheLine.hs
@@ -32,11 +32,6 @@ import System.Console.CmdTheLine.ArgVal
 import System.Console.CmdTheLine.Err
 import System.Console.CmdTheLine.Util
 
-import Text.PrettyPrint ( Doc )
-
-import Control.Monad    ( join )
-import Control.Monad.Trans.Error ( throwError )
-
 {-$term
 
   CmdTheLine is centered around the 'Term' Applicative Functor.  It allows us

src/System/Console/CmdTheLine/Arg.hs
@@ -29,7 +29,6 @@ import System.Console.CmdTheLine.Common  hiding ( Arg )
 import System.Console.CmdTheLine.CmdLine ( optArg, posArg )
 import System.Console.CmdTheLine.ArgVal  ( ArgVal, pp, parser )
 import qualified System.Console.CmdTheLine.Err  as E
-import qualified System.Console.CmdTheLine.Trie as T
 
 import Control.Applicative
 import Control.Arrow       ( second )
@@ -38,7 +37,7 @@ import Control.Monad.Trans.Error ( throwError )
 
 import Text.PrettyPrint
 
-import Data.List     ( sort, sortBy )
+import Data.List     ( sortBy )
 import Data.Function ( on )
 
 argFail :: Doc -> Err a
@@ -122,6 +121,7 @@ mkInfo names = ArgInfo
 --
 -- It is considered a programming error to provide an empty list of names to
 -- optInfo.
+optInfo :: [String] -> OptInfo
 optInfo [] =
   error "System.Console.CmdTheLine.Arg.optInfo recieved empty list of names."
 optInfo names = OInf (mkInfo names) "" "" "OPTIONS"
@@ -422,6 +422,7 @@ revPosRight = posList . PosR True
 -- Arguments as terms.
 --
 
+absent :: [ArgInfo] -> [ArgInfo]
 absent = map (\ ai -> ai { absence = Absent })
 
 -- | 'value' @arg@ makes @arg@ into a 'Term'.

src/System/Console/CmdTheLine/ArgVal.hs
@@ -19,7 +19,7 @@ module System.Console.CmdTheLine.ArgVal
   , pair, triple, quadruple, quintuple
   ) where
 
-import System.Console.CmdTheLine.Common ( splitOn, select )
+import System.Console.CmdTheLine.Common ( splitOn, select, HelpFormat(..) )
 import qualified System.Console.CmdTheLine.Err as E
 import qualified System.Console.CmdTheLine.Trie as T
 
@@ -55,10 +55,13 @@ parser = fst converter
 pp     :: ArgVal a => ArgPrinter a
 pp = snd converter
 
+decPoint, digits, sign :: Parsec String () String
 decPoint      = string "."
 digits        = many1 digit
-concatParsers = foldl (liftA2 (++)) $ return []
 sign          = option "" $ string "-"
+
+concatParsers :: [Parsec String () String] -> Parsec String () String
+concatParsers = foldl (liftA2 (++)) $ return []
 
 pInteger  :: ( Read a, Integral a ) => Parsec String () a
 pFloating :: ( Read a, Floating a ) => Parsec String () a
@@ -224,6 +227,7 @@ quintuple sep = ( parser', pp' )
     split []  = Nothing
     split str = Just $ splitOn sep str
 
+invalidVal :: String -> String -> Doc
 invalidVal = E.invalidVal `on` text
 
 instance ArgVal Bool where
@@ -292,4 +296,13 @@ instance ArgVal (Ratio Integer) where
         invalidVal str "expected a ratio in the form '<numerator> % <denominator>'"
 
 instance ArgVal (Maybe (Ratio Integer)) where
+  converter = just
+
+instance ArgVal HelpFormat where
+  converter = enum [ ( "pager", Pager )
+                   , ( "plain", Plain )
+                   , ( "groff", Groff )
+                   ]
+
+instance ArgVal (Maybe HelpFormat) where
   converter = just

src/System/Console/CmdTheLine/CmdLine.hs
@@ -20,8 +20,6 @@ import qualified System.Console.CmdTheLine.Trie as T
 import qualified Data.Map as M
 
 import Data.List     ( sort )
-import Data.Function ( on )
-
 
 optArg :: CmdLine -> ArgInfo -> [( Int, String, Maybe String )]
 optArg cl ai = case M.lookup ai cl of

src/System/Console/CmdTheLine/Common.hs
@@ -6,6 +6,7 @@ module System.Console.CmdTheLine.Common where
 
 import Data.Function    ( on )
 import Text.PrettyPrint ( Doc, text )
+import Control.Applicative ( Applicative(..) )
 
 import qualified Data.Map as M
 
@@ -129,6 +130,7 @@ data TermInfo = TermInfo
   } deriving ( Eq )
 
 -- | A default 'TermInfo'.
+defTI :: TermInfo
 defTI = TermInfo
   { termName  = ""
   , version   = ""
@@ -167,6 +169,19 @@ type Yield a = EvalInfo -> CmdLine -> Err a
 -- in the context of being computed from the command line arguments.
 data Term a = Term [ArgInfo] (Yield a)
 
+instance Functor Term where
+  fmap = yield . result . result . fmap
+    where
+    yield f (Term ais y) = Term ais (f y)
+    result = (.)
+
+instance Applicative Term where
+  pure v = Term [] (\ _ _ -> return v)
+
+  (Term args f) <*> (Term args' v) = Term (args ++ args') wrapped
+    where
+    wrapped ei cl = f ei cl <*> v ei cl
+
 
 data EvalKind = Simple   -- The program has no commands.
               | Main     -- The default program is running.
@@ -181,6 +196,7 @@ evalKind ei
 descCompare :: Ord a => a -> a -> Ordering
 descCompare = flip compare
 
+splitOn :: Eq a => a -> [a] -> ( [a], [a] )
 splitOn sep xs = ( left, rest' )
   where
   rest' = if rest == [] then rest else tail rest -- Skip the 'sep'.

src/System/Console/CmdTheLine/Err.hs
@@ -8,7 +8,6 @@ import System.Console.CmdTheLine.Common
 import qualified System.Console.CmdTheLine.Help as H
 
 import Text.PrettyPrint
-import Data.List ( intersperse )
 
 import Control.Monad ( join )
 import Control.Monad.Trans.Error
@@ -40,52 +39,74 @@ ret (Term ais yield) = Term ais yield'
 hsepMap :: (a -> Doc) -> [a] -> Doc
 hsepMap f = hsep . map f
 
-errArgv     = text "argv array must have at least one element"
+errArgv :: Doc
+errArgv = text "argv array must have at least one element"
+
+errNotOpt, errNotPos :: String
 errNotOpt   = "Option argument without name"
 errNotPos   = "Positional argument with a name"
+
+errHelp :: Doc -> Doc
 errHelp doc = text "term error, help requested for unknown command" <+> doc
 
 
+alts :: [String] -> Doc
 alts []    = error "alts called on empty list"
-alts [x]   = error "alts called on singleton list"
+alts [_]   = error "alts called on singleton list"
 alts [x,y] = hsepMap text [ "either", x, "or", y ]
 alts xs    = text "one of:" <+> fsep (punctuate (char ',') (map text xs))
 
+invalid :: String -> Doc -> Doc -> Doc
 invalid kind s exp = hsep
   [ text "invalid", text kind, quotes s<>char ',', exp ]
 
+invalidVal :: Doc -> Doc -> Doc
 invalidVal = invalid "value"
 
+no :: String -> String -> Doc
 no kind s = sep [ text "no such", text kind, quotes $ text s ]
 
+notDir :: Doc -> Doc
 notDir  s = quotes (s) <+> text "is not a directory"
 
+isDir :: Doc -> Doc
 isDir   s = quotes (s) <+> text "is a directory"
 
+element :: String -> String -> Doc -> Doc
 element kind str exp = fsep
   [ text "invalid element in", text kind, parens . quotes $ text str, exp ]
 
+sepMiss :: Char -> String -> Doc
 sepMiss sep str = invalidVal (text str) $
   hsep [ text "missing a", quotes $ char sep, text "separator" ]
 
+unknown :: String -> String -> Doc
 unknown kind v = sep [ text "unknown", text kind, quotes $ text v ]
 
+ambiguous :: String -> String -> [String] -> Doc
 ambiguous kind s ambs = hsep
   [ text kind, quotes $ text s, text "ambiguous, could be", alts ambs ]
 
+posExcess :: [Doc] -> Doc
 posExcess excess = text "too many arguments, don't know what to do with"
                <+> hsepMap prep excess
   where
   prep = (<> text ",") . quotes
 
+flagValue :: String -> String -> Doc
 flagValue f v = hsep
   [ text "option", quotes $ text f
   , text "is a flag, it cannot take the argument", quotes $ text v
   ]
 
+optValueMissing :: String -> Doc
 optValueMissing f = hsep
   [ text "option", quotes $ text f, text "needs an argument" ]
+
+optParseValue :: String -> Doc -> Doc
 optParseValue f e = sep [ text "option" <+> (quotes (text f)<>char ':'), e ]
+
+optRepeated :: String -> String -> Doc
 optRepeated f f'
   | f == f' = hsep
     [ text "option", quotes $ text f, text "cannot be repeated" ]
@@ -117,6 +138,7 @@ argMissing ai
     longName (x : xs)
       | length x > 2 || xs == [] = x
       | otherwise                = longName xs
+    longName [] = undefined
 
 print :: Handle -> EvalInfo -> Doc -> IO ()
 print h ei e = hPrint h $ (text . termName . fst $ main ei) <> char ':' <+> e

commit 21f7b3f0324bb65ee67a00d4afa66d8bbb1f77b9

Fix broken $(b,...) substitution
It was broken in commit f5294f1b56dce20a1ffef998441c8407c744811f

src/System/Console/CmdTheLine/Manpage.hs
@@ -56,7 +56,7 @@ substitute esc assoc = either (error . show) id . parse subst ""
     where
     content = try escape <|> choice replacers <|> safeChars
 
-  escape = esc <$> (anyChar <* char ',') <*> try replace <|> safeChars
+  escape = esc <$> (anyChar <* char ',') <*> (try replace <|> safeChars)
 
   safeChars = many1 $ satisfy (/= ')')
 

test/Main.hs
@@ -16,6 +16,7 @@ import System.Directory ( getTemporaryDirectory )
 import System.FilePath  ( (</>) )
 
 import System.Console.CmdTheLine ( unwrap, unwrapChoice, EvalExit )
+import System.Console.CmdTheLine.Manpage ( substitute, plainEsc )
 
 import Control.Applicative ( (<$>) )
 
@@ -104,6 +105,11 @@ tests =
       isRight <$> unwrapCP [ "-d", "cmdtheline.cabal", "LICENSE", "test" ]
     , testCase " w/ good args one" . assert $
       isRight <$> unwrapCP [ "-d", "cmdtheline.cabal", "foo" ]
+    ]
+
+  , testGroup "unittests"
+    [ testCase " escaping $(i,...)" . assert $ substitute plainEsc [] "$(i,foo)" == "foo"
+    , testCase " escaping $(b,...)" . assert $ substitute plainEsc [] "$(b,foo)" == "foo"
     ]
   ]
 